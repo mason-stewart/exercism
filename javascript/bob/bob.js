9f00158d95ca337cd4deaf86014adfa79fdc50bc
Mason Stewart <masondesu@gmail.com>
2014-03-01 12:05:33 -0500

    Updated bob.js based on feedback, yay!

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,23 +1,29 @@
 var Bob = function() {
   this.hey = function(phrase){
 
+    // regexes
+    var emptyRegex = /\w+/,
+        uppercaseRegex = /[A-Z]+/,
+        questionRegex = /\?$/;
+
+
     // First check if it's empty, or is just whitespace
-    if (!/\w+/.exec(phrase)) {
+    if (!emptyRegex.test(phrase)) {
       return 'Fine. Be that way!';
 
     // Does it have lower case letters?
-    // If not, the first regex's return value will match the entire phrase.
+    // If not, the .toUpperCase() return value will match the entire phrase.
     // Then check that we have at least one uppercase letter.
-    } else if (/[^a-zöäü]+/.exec(phrase)[0] === phrase && /[A-Z]+/.exec(phrase)) {
+    } else if (phrase.toUpperCase() === phrase && uppercaseRegex.test(phrase)) {
       return 'Woah, chill out!';
 
     // Does it end with a question mark?
-    } else if (phrase.slice(-1)[0] === '?') {
+    } else if (questionRegex.test(phrase)) {
       return 'Sure.';
 
     // Whatever ;D
     } else {
-      return 'Whatever.';   
+      return 'Whatever.';
     }
   };
 };
